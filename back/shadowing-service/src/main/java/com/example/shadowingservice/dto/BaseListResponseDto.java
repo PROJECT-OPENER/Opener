eb17e955d6d7c7e915830e291410d096e79fe078
이우승 <ss6116@daum.net>
2023-04-27 17:12:39 +0900

    S08P31C104-305 init : package 구조 생성 1. package 구조 생성

---
+++ back/shadowing-service/src/main/java/com/example/shadowingservice/dto/BaseListResponseDto.java
@@ -0,0 +1,24 @@
+package com.example.shadowingservice.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseListResponseDto<T> {
+
+	private Integer code;
+
+	private String message;
+
+	private List<T> data;
+
+	public BaseListResponseDto(int code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+}
