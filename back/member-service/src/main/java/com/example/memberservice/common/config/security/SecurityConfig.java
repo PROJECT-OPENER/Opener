2fbe148c1ba19a7ea74d3aba56f538792e084df2
김윤미 <yoonmihy@naver.com>
2023-05-02 13:33:20 +0900

    S08P31C104-471 fix : Cors Error 1. Cors error 고치는 중

--- back/member-service/src/main/java/com/example/memberservice/common/config/security/SecurityConfig.java
+++ back/member-service/src/main/java/com/example/memberservice/common/config/security/SecurityConfig.java
@@ -30,6 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.cors()
+			.and()
 			.csrf().disable()
 
 			// .exceptionHandling()
