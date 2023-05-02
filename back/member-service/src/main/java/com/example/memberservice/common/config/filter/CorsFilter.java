a6fc7ff5a52f2e217050ce1de1fe966ca068a8d8
김윤미 <yoonmihy@naver.com>
2023-05-02 11:47:42 +0900

    S08P31C104-471 fix : Cors Error 1. Cors Filter 수정

--- back/member-service/src/main/java/com/example/memberservice/common/config/filter/CorsFilter.java
+++ back/member-service/src/main/java/com/example/memberservice/common/config/filter/CorsFilter.java
@@ -28,12 +28,21 @@ public class CorsFilter implements Filter {
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws
 		IOException,
 		ServletException {
+		HttpServletRequest request = (HttpServletRequest)servletRequest;
 		HttpServletResponse response = (HttpServletResponse)servletResponse;
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH, PUT");
+
+		response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); //dev
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, PUT, OPTIONS");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-		filterChain.doFilter(servletRequest, servletResponse);
+		response.setHeader("Access-Control-Allow-Headers",
+			"Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+			response.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			filterChain.doFilter(servletRequest, servletResponse);
+		}
 	}
 
 	@Override
