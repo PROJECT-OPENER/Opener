6e432c93c89bce4767ade943c1d4da8e9a4f6c83
김윤미 <yoonmihy@naver.com>
2023-05-02 12:00:24 +0900

    S08P31C104-471 fix : Cors Error 1. Cors Filter 수정

--- back/member-service/src/main/java/com/example/memberservice/common/config/filter/CorsFilter.java
+++ back/member-service/src/main/java/com/example/memberservice/common/config/filter/CorsFilter.java
@@ -35,8 +35,10 @@ public class CorsFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, PUT, OPTIONS");
 		response.setHeader("Access-Control-Max-Age", "3600");
+		// response.setHeader("Access-Control-Allow-Headers",
+		// 	"Origin, X-Requested-With, Content-Type, Accept, Authorization");
 		response.setHeader("Access-Control-Allow-Headers",
-			"Origin, X-Requested-With, Content-Type, Accept, Authorization");
+			"*");
 
 		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
 			response.setStatus(HttpServletResponse.SC_OK);
