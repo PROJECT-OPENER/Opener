5633e94cacc883d21e08c4c455cb9aea5320187a
김윤미 <yoonmihy@naver.com>
2023-05-02 09:05:14 +0900

    S08P31C104-471 fix : entity 수정 1. member service 엔티티 수정 2. 패키지 구조 수정

--- back/member-service/src/main/java/com/example/memberservice/entity/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.example.memberservice.entity;
-
-public enum Gender {
-	MALE, FEMALE
-}
--- back/member-service/src/main/java/com/example/memberservice/entity/member/MemberInterest.java
+++ back/member-service/src/main/java/com/example/memberservice/entity/member/MemberInterest.java
@@ -1,4 +1,4 @@
-package com.example.memberservice.entity;
+package com.example.memberservice.entity.member;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.example.memberservice.entity.BaseEntity;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -21,26 +23,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-@Table(name = "badge")
-public class Badge extends BaseEntity {
+@Table(name = "memberInterest")
+public class MemberInterest extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "badge_id")
-	private Long badgeId;
+	@Column(name = "member_interest_id")
+	private Long memberInterestId;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id", nullable = false)
 	private Member member;
 
-	@Column(name = "attendance_count", nullable = false)
-	private int attendanceCount;
-
-	@Column(name = "shadowing_count", nullable = false)
-	private int shadowingCount;
-
-	@Column(name = "challenge_count", nullable = false)
-	private int challengeCount;
-
-	@Column(name = "game_count", nullable = false)
-	private int gameCount;
+	@Column(name = "interest_id", nullable = false)
+	private Long interestId;
 }
