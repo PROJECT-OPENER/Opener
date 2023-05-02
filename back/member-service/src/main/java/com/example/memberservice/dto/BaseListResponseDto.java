6a946b2f97f697f8e5f4266cd7a4a53e162d805d
김윤미 <yoonmihy@naver.com>
2023-05-02 09:30:09 +0900

    S08P31C104-471 feat : BaseResponse dto 설정 1. BaseResponseDto 추가 2. BaseListResponseDto 추가

---
+++ back/member-service/src/main/java/com/example/memberservice/dto/BaseListResponseDto.java
@@ -0,0 +1,25 @@
+package com.example.memberservice.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseListResponseDto<T> {
+
+	private Integer code;
+
+	private String message;
+
+	private List<T> data;
+
+	public BaseListResponseDto(int code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+}
+
