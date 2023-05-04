2856d15af083a7fd88730ff513e5d82d60576b21
김윤미 <yoonmihy@naver.com>
2023-05-04 17:22:53 +0900

    S08P31C104-471 feat : api-gateway 로그인 사용자 정보 설정 1. apigateway-service에서 로그인한 사용자의 정보를 헤더에 담아 micro service들로 전송

--- back/apigateway-service/src/main/java/com/example/apigatewayservice/config/CorsConfig.java
+++ back/apigateway-service/src/main/java/com/example/apigatewayservice/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.example.apigatewayservice.filter;
+package com.example.apigatewayservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
