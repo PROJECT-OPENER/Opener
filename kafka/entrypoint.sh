625d172f7a1c3b7bf2155548692bf89b6127b0f0
김윤미 <yoonmihy@naver.com>
2023-05-05 15:56:24 +0900

    S08P31C104-471 fix : kafka Dockerfile 수정 1. 파일 경로 수정

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # create-kafka-topic.sh 스크립트를 사용하여 토픽을 생성합니다.
-/create-kafka-topic.sh member 3 2
+/usr/bin/create-kafka-topic.sh member 3 2
 
 # Kafka 컨테이너를 실행합니다.
-/docker-entrypoint.sh kafka-server-start.sh /etc/kafka/config/server.properties
+/usr/bin/docker-entrypoint.sh kafka-server-start.sh /opt/kafka/config/server.properties
