b56a354360cc03c73817e12ef1b5ff5be9f9d24a
김윤미 <yoonmihy@naver.com>
2023-05-05 18:22:27 +0900

    S08P31C104-471 fix : kafka Dockerfile 수정 1. kafka config 수정

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -6,12 +6,12 @@ echo "RUN KAFKA"
 chmod +x /opt/kafka/bin/kafka-server-start.sh
 /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties &
 
-# Kafka 서버가 실행될 때까지 대기
-while ! nc -z k8c104.p.ssafy.io 9092; do
-  sleep 1
-done
+# # Kafka 서버가 실행될 때까지 대기
+# while ! nc -z k8c104.p.ssafy.io 9092; do
+#   sleep 1
+# done
 
-echo "CREATE TOPIC"
+# echo "CREATE TOPIC"
 
-# create-kafka-topic.sh 스크립트를 사용하여 토픽을 생성합니다.
-/usr/bin/create-kafka-topic.sh member_topic 4
+# # create-kafka-topic.sh 스크립트를 사용하여 토픽을 생성합니다.
+# /usr/bin/create-kafka-topic.sh member_topic 4
