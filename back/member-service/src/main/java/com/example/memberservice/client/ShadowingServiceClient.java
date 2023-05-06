231f2aea993749810c5326679682e06efe2dc8d3
김윤미 <yoonmihy@naver.com>
2023-05-07 00:27:04 +0900

    S08P31C104-471 fix : 로그인 시 관심사 정보 추가 1. 로그인 시도 사용자의 관심사 조회

--- back/member-service/src/main/java/com/example/memberservice/client/ShadowingServiceClient.java
+++ back/member-service/src/main/java/com/example/memberservice/client/ShadowingServiceClient.java
@@ -1,17 +1,17 @@
 package com.example.memberservice.client;
 
+import java.util.Optional;
+
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.memberservice.client.dto.response.InterestResponseDto;
 import com.example.memberservice.dto.BaseResponseDto;
 
 @FeignClient(name = "shadowing-service")
 public interface ShadowingServiceClient {
-	/**
-	 * TODO: Feign Client API 생성
-	 */
-	@GetMapping("")
-	BaseResponseDto<InterestResponseDto> getInterest();
+
+	@GetMapping("/interests/{interest-id}")
+	Optional<BaseResponseDto<InterestResponseDto>> getInterest(@PathVariable(value = "interest-id") Long interestId);
 }
--- back/member-service/src/main/java/com/example/memberservice/repository/MemberInterestRepository.java
+++ back/member-service/src/main/java/com/example/memberservice/repository/MemberInterestRepository.java
@@ -1,8 +1,12 @@
 package com.example.memberservice.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.memberservice.entity.member.Member;
 import com.example.memberservice.entity.member.MemberInterest;
 
 public interface MemberInterestRepository extends JpaRepository<MemberInterest, Long>, MemberRepositoryCustom {
+	List<MemberInterest> findAllByMember(Member member);
 }
