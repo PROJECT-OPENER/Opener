1e959d55e1f947722aa03632fa916cd91902962b
daydeuk <daydeuk@naver.com>
2023-05-10 15:49:15 +0900

    S08P31C104-579 feat : send-message API
    1. socket 통신 send-message API 개발

--- back/chatting-service/src/main/java/com/example/chattingservice/controller/AuthChattingController.java
+++ back/chatting-service/src/main/java/com/example/chattingservice/controller/AuthChattingController.java
@@ -1,10 +1,12 @@
 package com.example.chattingservice.controller;
 
+import com.example.chattingservice.dto.request.SendMessageRequestDto;
 import com.example.chattingservice.service.ChattingServiceImpl;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.messaging.handler.annotation.*;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthChattingController {
 
 	private final ChattingServiceImpl chattingService;
+	private final SimpMessagingTemplate messagingTemplate;
 
 	@MessageMapping("/user-chat")
 	public void createWaiting(@Header("Authorization") String token, @Payload String message) {
 		chattingService.createWaiting(token);
+	}
+
+	/**
+	 * 송신
+	 * @param sendMessageRequestDto
+	 */
+	@MessageMapping("/user-chat/rooms/{room-id}")
+	public void sendMessage(SendMessageRequestDto sendMessageRequestDto) {
+		messagingTemplate.convertAndSend("/sub/user-chat/" + sendMessageRequestDto.getRoomId(),
+			sendMessageRequestDto);
 	}
 }
