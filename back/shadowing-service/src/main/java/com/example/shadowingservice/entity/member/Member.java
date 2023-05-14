53a0a325cbee6261f238a99bce0cf8176fad4e5e
이우승 <ss6116@daum.net>
2023-05-14 15:18:43 +0900

    S08P31C104-609 feat : member-service entitiy 생성 1. member-service entitiy 생성

--- back/shadowing-service/src/main/java/com/example/shadowingservice/entity/member/Member.java
+++ back/shadowing-service/src/main/java/com/example/shadowingservice/entity/member/Member.java
@@ -12,7 +12,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
 import org.hibernate.annotations.DynamicUpdate;
 
 import com.example.shadowingservice.entity.BaseEntity;
