eac854cca28c78eb430fb8fb694989ee4260f319
daydeuk <daydeuk@naver.com>
2023-05-16 19:57:56 +0900

    S08P31C104-765 feat : isLike JsonProperty 추가
    1. isLIke JsonProperty 추가

--- back/challenge-service/src/main/java/com/example/challengeservice/dto/response/SelectMemberChallengeResponseDto.java
+++ back/challenge-service/src/main/java/com/example/challengeservice/dto/response/SelectMemberChallengeResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.challengeservice.dto.response;
 
 import com.example.challengeservice.entity.challenge.MemberChallenge;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,7 +15,8 @@ public class SelectMemberChallengeResponseDto {
     private Long memberChallengeId;
     private String memberChallengeNickname;
     private String memberChallengeUrl;
-    boolean isLike;
+    @JsonProperty(value = "isLike")
+    private Boolean isLike;
 
     public static SelectMemberChallengeResponseDto from(MemberChallenge memberChallenge, int isLove) {
         boolean isLike = true;
