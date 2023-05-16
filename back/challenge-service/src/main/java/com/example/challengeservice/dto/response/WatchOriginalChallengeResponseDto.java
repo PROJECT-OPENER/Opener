739d186a689bc15c49810c3f17d825c6a38d0fd2
daydeuk <daydeuk@naver.com>
2023-05-16 13:34:00 +0900

    S08P31C104-765 feat : challenge-caption
    1. challenge entity 자막 컬럼 추가

--- back/challenge-service/src/main/java/com/example/challengeservice/dto/response/WatchOriginalChallengeResponseDto.java
+++ back/challenge-service/src/main/java/com/example/challengeservice/dto/response/WatchOriginalChallengeResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.challengeservice.dto.response;
 
 import com.example.challengeservice.entity.challenge.Challenge;
+
 import lombok.*;
 
 @Getter
@@ -8,22 +9,24 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class WatchOriginalChallengeResponseDto {
-    private Long challengeId;
-    private String title;
-    private String challengeUrl;
-    private String startTime;
-    private String endTime;
-    private int joinCount;
+	private Long challengeId;
+	private String title;
+	private String challengeUrl;
+	private String startTime;
+	private String endTime;
+	private String caption;
+	private int joinCount;
 
-    public static WatchOriginalChallengeResponseDto from(Challenge challenge, int joinCount) {
-        return WatchOriginalChallengeResponseDto.builder()
-                .challengeId(challenge.getChallengeId())
-                .title(challenge.getTitle())
-                .challengeUrl(challenge.getChallengeUrl())
-                .startTime(challenge.getStartTime())
-                .endTime(challenge.getEndTime())
-                .joinCount(joinCount)
-                .build();
-    }
+	public static WatchOriginalChallengeResponseDto from(Challenge challenge, int joinCount) {
+		return WatchOriginalChallengeResponseDto.builder()
+			.challengeId(challenge.getChallengeId())
+			.title(challenge.getTitle())
+			.challengeUrl(challenge.getChallengeUrl())
+			.startTime(challenge.getStartTime())
+			.endTime(challenge.getEndTime())
+			.caption(challenge.getCaption())
+			.joinCount(joinCount)
+			.build();
+	}
 
 }
