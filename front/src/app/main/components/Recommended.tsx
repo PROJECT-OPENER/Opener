8b3b930dbe6294fa7e350cd30a2f57d7a73ef395
moonthree <fpdhslr0725@gmail.com>
2023-05-17 21:07:03 +0900

    S08P31C104-781 fix : recommend

--- front/src/app/main/components/Recommended.tsx
+++ front/src/app/main/components/Recommended.tsx
@@ -15,13 +15,18 @@ interface dataInterface {
 const Recommended = () => {
   const [data, setData] = useState<dataInterface[]>();
   const getRecommendList = async () => {
-    const res = await getRecommendListApi();
-    console.log(res.data);
-    setData(res.data);
+    try {
+      const res = await getRecommendListApi();
+      console.log(res.data);
+      setData(res.data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   useEffect(() => {
     getRecommendList();
+    console.log(data);
   }, []);
 
   const options = {
@@ -30,80 +35,84 @@ const Recommended = () => {
     wheelPropagation: true,
     minScrollbarLength: 2,
   };
+
   return (
     <div className="mt-10">
       {/* 데스크탑 용 */}
+      {/* next.config.js에서 remotePatterns안에 user-images.githubusercontent.com 삭제해야함 */}
       <div className="hidden lg:block">
         <div className="flex flex-row justify-between mb-3">
           <h1 className="text-lg">추천 문장</h1>
           <button>더 보기</button>
         </div>
-        <div className="hidden lg:flex flex-row justify-between w-full h-full">
-          {data?.slice(0, 3).map((content, index) => {
-            return (
-              <Link
-                href={'/shadowing/learning/' + content.videoId}
-                key={index}
-                className="opacity-90 hover:opacity-100 shadow-custom w-[320px] h-[300px] p-4 rounded-3xl bg-[#ffffff] hover:shadow-customhover"
-              >
-                {/* next.config.js에서 remotePatterns안에 user-images.githubusercontent.com 삭제해야함 */}
-                <Image
-                  src={content.thumbnailUrl}
-                  alt=""
-                  width={288}
-                  height={188}
-                  className="relative rounded-xl overflow-hidden"
-                />
-                <div className="mt-4 p-1">
-                  <p className="text-base font-semibold mb-1">
-                    {content.engSentence}
-                  </p>
-                  <p className="text-sm font-light">{content.korSentence}</p>
-                </div>
-              </Link>
-            );
-          })}
-        </div>
-      </div>
-
-      {/* 모바일 용 */}
-      <div className="lg:hidden overflow-hidden">
-        <h1 className="text-lg mb-3 ml-4">추천 문장</h1>
-        <PerfectScrollbar options={options} className="w-full">
-          <div className="flex flex-row relative w-full p-4">
-            {data?.map((content, index) => {
+        {/* <div className="hidden lg:flex flex-row justify-between w-full h-full">
+          {data &&
+            data?.slice(0, 3).map((content, index) => {
               return (
                 <Link
                   href={'/shadowing/learning/' + content.videoId}
                   key={index}
-                  className="shadow-custom mr-4 p-3 rounded-3xl bg-[#ffffff] hover:shadow-customhover"
+                  className="opacity-90 hover:opacity-100 shadow-custom w-[320px] h-[300px] p-4 rounded-3xl bg-[#ffffff] hover:shadow-customhover"
                 >
-                  {/* next.config.js에서 remotePatterns안에 user-images.githubusercontent.com 삭제해야함 */}
-                  <div className="relative w-[254px]">
-                    <img
-                      src={content.thumbnailUrl}
-                      alt=""
-                      className="w-full rounded-xl"
-                    />
-                    <div className="mt-2 px-1">
-                      <p className="text-base font-semibold mb-1">
-                        {content.engSentence}
-                      </p>
-                      <p className="text-sm font-light">
-                        {content.korSentence}
-                      </p>
-                    </div>
+                  <Image
+                    src={content.thumbnailUrl}
+                    alt=""
+                    width={288}
+                    height={188}
+                    className="relative rounded-xl overflow-hidden"
+                  />
+                  <div className="mt-4 p-1">
+                    <p className="text-base font-semibold mb-1">
+                      {content.engSentence}
+                    </p>
+                    <p className="text-sm font-light">{content.korSentence}</p>
                   </div>
                 </Link>
               );
             })}
+        </div> */}
+      </div>
+
+      {/* 모바일 용 */}
+
+      {/* next.config.js에서 remotePatterns안에 user-images.githubusercontent.com 삭제해야함 */}
+      <div className="lg:hidden overflow-hidden">
+        <h1 className="text-lg mb-3 ml-4">추천 문장</h1>
+        {/* <PerfectScrollbar options={options} className="w-full">
+          <div className="flex flex-row relative w-full p-4">
+            {data &&
+              data?.map((content, index) => {
+                return (
+                  <Link
+                    href={'/shadowing/learning/' + content.videoId}
+                    key={index}
+                    className="shadow-custom mr-4 p-3 rounded-3xl bg-[#ffffff] hover:shadow-customhover"
+                  >
+                    <div className="relative w-[254px]">
+                      <img
+                        src={content.thumbnailUrl}
+                        alt=""
+                        className="w-full rounded-xl"
+                      />
+                      <div className="mt-2 px-1">
+                        <p className="text-base font-semibold mb-1">
+                          {content.engSentence}
+                        </p>
+                        <p className="text-sm font-light">
+                          {content.korSentence}
+                        </p>
+                      </div>
+                    </div>
+                  </Link>
+                );
+              })}
             <div>
               <button className="shadow-custom mr-2 w-[110px] h-[250px] sm:h-[285px] rounded-3xl bg-[#fff] hover:text-white active:text-white hover:bg-brandP active:bg-[#620fcf]">
                 더 보기
               </button>
             </div>
           </div>
-        </PerfectScrollbar>
+        </PerfectScrollbar> */}
       </div>
     </div>
   );
