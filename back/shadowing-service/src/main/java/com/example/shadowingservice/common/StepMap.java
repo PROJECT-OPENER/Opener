3827f2a63dbb470dbdb2d8f2bb27fdfcd4a2356c
이우승 <ss6116@daum.net>
2023-05-17 21:53:51 +0900

    S08P31C104-778 refactor : category hashMap 수정 1. category hashMap 수정

--- back/shadowing-service/src/main/java/com/example/shadowingservice/common/StepMap.java
+++ back/shadowing-service/src/main/java/com/example/shadowingservice/common/StepMap.java
@@ -10,10 +10,14 @@ public class StepMap {
 		hashMap = new HashMap<>();
 
 		// 여기서 초기값을 설정합니다.
-		hashMap.put(1, "아이브");
-		hashMap.put(2, "뉴진스");
-		hashMap.put(3, "엔믹스");
-		hashMap.put(4, "블랙핑크");
+		hashMap.put(1, "영화");
+		hashMap.put(2, "여행");
+		hashMap.put(3, "음악");
+		hashMap.put(4, "스포츠");
+		hashMap.put(5, "음식");
+		hashMap.put(6, "일상");
+		hashMap.put(7, "애니메이션");
+		hashMap.put(8, "비즈니스");
 	}
 
 	public static StepMap getInstance() {
