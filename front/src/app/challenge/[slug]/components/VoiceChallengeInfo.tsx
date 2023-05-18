6cb4702eed28b2bf1a07e4b7c02700b41d4aa0bf
itmakesmesoft <dmsgur7112@gmail.com>
2023-05-18 21:04:48 +0900

    S08P31C104-452 fix : 챌린지 디자인 일부 수정

--- front/src/app/challenge/[slug]/components/VoiceChallengeInfo.tsx
+++ front/src/app/challenge/[slug]/components/VoiceChallengeInfo.tsx
@@ -2,48 +2,47 @@ import React from 'react';
 import { FiUserPlus } from 'react-icons/fi';
 import { originalChallenge } from '@/types/share';
 import Link from 'next/link';
-import { AiOutlineCamera } from 'react-icons/ai';
-import { GrPlayFill } from 'react-icons/gr';
+import { BsPlay, BsCameraVideo } from 'react-icons/bs';
 type Props = {
   originalChallengeResponseDto: originalChallenge;
 };
 
 const VoiceChallengeInfo = ({ originalChallengeResponseDto }: Props) => {
   return (
-    <div className="bg-white p-5 grid md:grid-cols-5  rounded-lg">
-      <div className="mx-5 flex items-center justify-center md:col-span-2">
+    <div className="bg-white rounded-lg px-5 relative flex py-5 flex-col sm:flex-row items-center justify-center">
+      <div className="sm:mx-5">
         <Link
           href={`challenge/${originalChallengeResponseDto.challengeId}/original`}
         >
           <img
-            className="w-auto rounded-lg bg-gray-200"
+            className="rounded-lg bg-gray-200"
             src={`https://img.youtube.com/vi/${originalChallengeResponseDto.challengeUrl}/0.jpg`}
             alt="..."
           />
         </Link>
       </div>
-      <div className="md:col-span-3 flex flex-col justify-center mx-2">
+      <div className="flex flex-col sm:min-w-[300px] justify-center sm:mx-5">
         <div className="my-5 text-lg font-medium mx-2">
           <p>{originalChallengeResponseDto.title}</p>
         </div>
         <div className="flex items-center mb-5 text-lg font-medium mx-3">
-          <FiUserPlus size={'1.5rem'} className="mr-2" />
+          <FiUserPlus size={'1.3rem'} className="mr-2" />
           {originalChallengeResponseDto.joinCount}
         </div>
         <div className="flex justify-center">
           <Link
             href={`challenge/${originalChallengeResponseDto.challengeId}/original`}
-            className="bg-[#F0F0F0] w-48 py-3 rounded-3xl flex justify-center items-center shadow-sm mx-2"
+            className="bg-white text-base font-semibold rounded-3xl flex justify-center items-center shadow-custom px-3 py-1 hover:bg-brandP active:bg-[#7113ea] hover:text-white mr-3"
           >
-            <GrPlayFill size={'1.3rem'} className="fill-black mr-2" />
-            <p className="text-lg font-bold">미리보기</p>
+            <BsPlay className="mr-2" />
+            미리보기
           </Link>
           <Link
             href={`challenge/shooting/${originalChallengeResponseDto.challengeId}`}
-            className="bg-[#F0F0F0] w-48 py-3 rounded-3xl flex justify-center items-center sshadow-sm mx-2"
+            className="bg-white text-base font-semibold rounded-3xl flex justify-center items-center shadow-custom px-3 py-1 hover:bg-brandP active:bg-[#7113ea] hover:text-white"
           >
-            <AiOutlineCamera size={'1.5rem'} className="fill-black mr-2" />
-            <p className="text-lg font-bold">참여하기</p>
+            <BsCameraVideo className="mr-2" />
+            참여하기
           </Link>
         </div>
       </div>
